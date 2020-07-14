ae2253d14319b09309aeae408c2873ab40b2bb04
LassaFever <976690455@qq.com>
2020-07-14 17:30:45 +0800

    第五章上机作业(#5)

--- src/main/java/com/fish/YunpingtaiApplication.java
+++ src/main/java/com/fish/YunpingtaiApplication.java
@@ -3,14 +3,20 @@ package com.fish;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @ServletComponentScan
 @SpringBootApplication
-public class YunpingtaiApplication {
+public class YunpingtaiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(YunpingtaiApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(YunpingtaiApplication.class);
+    }
 }
